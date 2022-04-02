8108b5e23db311a4c44e0159969d7774e35b62dc
Bart Botta <00003b@gmail.com>
2022-04-02 11:29:40 -0500

    more stuff for debugging

--- ttf.lisp
+++ ttf.lisp
@@ -10,17 +10,25 @@
     (sdf/base::end-contour)))
 
 
+(defvar *dump-ttf-points* nil)
 (defun shape-from-glyph (glyph &key metadata)
-  (sdf/base::with-shape-builder (b :metadata metadata)
-    (zpb-ttf:do-contours (c glyph)
-      (let ((start t))
-        (zpb-ttf:do-contour-segments (p0 p1 p2) c
-          (when start
-            (setf start nil)
-            (sdf/base::start-contour (zpb-ttf:x p0) (zpb-ttf:y p0)))
-          (if p1
-              (sdf/base::quadratic-to (zpb-ttf:x p1) (zpb-ttf:y p1)
-                                      (zpb-ttf:x p2) (zpb-ttf:y p2))
-              (sdf/base::line-to (zpb-ttf:x p2) (zpb-ttf:y p2))))
-        (sdf/base::end-contour)))))
+  (flet ((xy (x) (when x (list (zpb-ttf:x x) (zpb-ttf:y x)))))
+   (sdf/base::with-shape-builder (b :metadata metadata)
+     (zpb-ttf:do-contours (c glyph)
+       (let ((start t))
+         (zpb-ttf:do-contour-segments (p0 p1 p2) c
+           (when start
+             (setf start nil)
+             (sdf/base::start-contour (zpb-ttf:x p0) (zpb-ttf:y p0))
+             (when *dump-ttf-points*
+              (format t "start contour ~s~%" (xy p0))))
+           (when *dump-ttf-points*
+             (format t " [~s] ~s ~s~%" (xy p0) (xy p1) (xy p2)))
+           (if p1
+               (sdf/base::quadratic-to (zpb-ttf:x p1) (zpb-ttf:y p1)
+                                       (zpb-ttf:x p2) (zpb-ttf:y p2))
+               (sdf/base::line-to (zpb-ttf:x p2) (zpb-ttf:y p2))))
+         (when *dump-ttf-points*
+           (format t "end contour~%"))
+         (sdf/base::end-contour))))))
 
